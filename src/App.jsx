f0ca9f9643dcdbde7e28db945e1c1494fa99bd78
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-17 20:40:58 +0530

    state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Footer from "./components/footer"
 import Header from "./components/header"
 import Profile from "./components/profile"
 
+
+
+
 function App() {
+ const [count,setCount]=useState(0);
   const userData={
     userName:"user123",
     eamil:"abs",
     city:"kandy"
 
   };
+  const handleClick=()=>{setCount((count+1))};
+
 
   return (
     <>
@@ -27,6 +34,14 @@ function App() {
 
       </Profile>
       <Header></Header>
+
+
+
+      <div>
+        <h3>{count}</h3>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+
     </>
   )
 }
