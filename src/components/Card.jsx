d521809ee61b776b5d50294fa19886e604a3738d
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-20 12:13:50 +0530

    added imge with mapping

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import './Card.css'
 
-function Card({userName,age,details}) {
+function Card({userName,imageUrl,age,details}) {
     return (  
         <>
           <div className="card-bg">
                <div className="card-name">{userName}</div>
+               <img src={imageUrl} alt="userPhoto" className='card-img'/>
                <div className="card-age">{age}</div>
                <div className="card-details">{details}</div>
           </div>
